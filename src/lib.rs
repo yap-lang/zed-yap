4597af6cd6f4db02f077222e48192e3554d2ed08
valaphee <32491319+valaphee@users.noreply.github.com>
2025-06-30 18:56:43 +0200

    update grammar, support lsp

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,18 @@ impl zed::Extension for YapExtension {
     fn new() -> Self {
        Self
     }
+
+    fn language_server_command(
+        &mut self,
+        _language_server_id: &zed::LanguageServerId,
+        _worktree: &zed::Worktree,
+    ) -> zed::Result<zed::Command> {
+        Ok(zed::Command {
+            command: "yap".into(),
+            args: vec!["lsp".into()],
+            env: vec![],
+        })
+    }
 }
 
 zed::register_extension!(YapExtension);
