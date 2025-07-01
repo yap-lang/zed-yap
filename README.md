cd702f44db39e730b9c0d9fe87fda8a65dfdfd65
valaphee <32491319+valaphee@users.noreply.github.com>
2025-07-01 20:19:37 +0200

    update grammar

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # zed-yap
 
 [![License](https://img.shields.io/github/license/yap-lang/zed-yap?style=for-the-badge)](LICENSE.txt)
+
+Extension for [Zed](https://github.com/zed-industries/zed) to support YAP!
--- languages/yap/highlights.scm
+++ languages/yap/highlights.scm
@@ -1,13 +1,14 @@
-(variable property: (_)? @property) @variable
-((variable) @type (#match? @type "^_*[A-Z][A-Za-z0-9_]*$"))
+(name field: (_)? @property) @variable
+((name) @type (#match? @type "^_*[A-Z][A-Za-z0-9_]*$"))
 (literal_number) @number
 (literal_string) @string
-(expression_call function: (_) @function.call)
+(expression_call args: (_) name: (_) @function.call)
 
 [
   "if"
   "elif"
   "else"
+  "for"
 ] @keyword
 
 [
